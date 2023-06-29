f67d8355d38e7fdf7c8fdbe77e31caf33cf4cd89
aaa <bbb>
2023-06-29 16:05:38 +0800

    M

--- music-box-avr32/src/ssc_i2s_example.c
+++ music-box-avr32/src/ssc_i2s_example.c
@@ -2,7 +2,10 @@
 #include "pm.h"
 #include "gpio.h"
 #include "ssc_i2s.h"
+#include "sysclk.h"
+#include "print_funcs.h"
 #include "user_board.h"
+#include <stdio.h>
 
 
 /*! \brief Main function, application starts executing here after
@@ -20,7 +23,19 @@ int main(void)
 	volatile avr32_ssc_t *ssc = &AVR32_SSC;
 
 	// Switch main clock to external oscillator 0 (crystal).
-	pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
+	//pm_switch_to_osc0(&AVR32_PM, FOSC0, OSC0_STARTUP);
+	
+	/*
+	 * The call to sysclk_init() will disable all non-vital
+	 * peripheral clocks, except for the peripheral clocks explicitly
+	 * enabled in conf_clock.h.
+	 */
+	sysclk_init();
+
+	/* Initialize the USART module to print trace messages */
+	init_dbg_rs232(sysclk_get_pba_hz());
+	
+	printf("Debug uart is working!");
 
 	// Assign GPIO to SSC.
 	gpio_enable_module(SSC_GPIO_MAP, sizeof(SSC_GPIO_MAP) / sizeof(SSC_GPIO_MAP[0]));
